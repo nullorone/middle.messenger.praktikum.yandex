11bb7f0c126101c33f4dd55e755093a25b0ded55
nullorone <sy.v@mail.ru>
2022-11-24 22:46:41 +0300

    Обновляет конфиги

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,32 @@ module.exports = {
     es2021: true
   },
   extends: 'standard-with-typescript',
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   parserOptions: {
-    project: 'tsconfig.json',
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
+      "@typescript-eslint/semi": [1, "always"],
+      "@typescript-eslint/space-before-function-paren": "off",
+      "@typescript-eslint/strict-boolean-expressions": "off",
+      "@typescript-eslint/indent": [
+          "error",
+          4,
+      ],
+      "@typescript-eslint/no-empty-interface": [
+          "error",
+          {
+              "allowSingleExtends": true
+          }
+      ],
+      "@typescript-eslint/no-use-before-define": "off",
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
   }
 }
